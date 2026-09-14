4ab5b8dbe6d06283d13a967ec69c5b5a453077fe
rogeriorodriguesdelima260-droid <rogeriorodriguesdelima260@gmail.com>
2026-09-14 20:30:49 -0300

    Add files via upload

---
+++ lista_poo3/lista_poo3/Quadrado.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista_poo3
+{
+    internal class Quadrado
+    {
+        // Atributos
+        private double diagonal;
+        private double area;
+
+        // Métodos construtores
+        public Quadrado()
+        {
+            this.diagonal = 0;
+            this.area = 0;
+        }
+        public Quadrado(double diagonal)
+        {
+            this.diagonal = diagonal;
+            this.area = 0;
+        }
+
+        // Métodos de interface (set/get)
+        public void setDiagonal(double diagonal)
+        {
+            this.diagonal = diagonal;
+        }
+        public double getDiagonal()
+        {
+            return this.diagonal;
+        }
+        public double getArea()
+        {
+            return this.area;
+        }
+        // Método funcional
+        public void calcular()
+        {
+            this.area = (this.diagonal * this.diagonal) / 2;
+        }
+    }
+}
