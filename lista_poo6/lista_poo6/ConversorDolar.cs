870726dafe8d652893771f8fcb626368bf395f1a
rogeriorodriguesdelima260-droid <rogeriorodriguesdelima260@gmail.com>
2026-09-14 20:41:08 -0300

    Add files via upload

---
+++ lista_poo6/lista_poo6/ConversorDolar.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista_poo6
+{
+    internal class ConversorDolar
+    {
+        // Atributos
+        private double cotacao;
+        private double quantidadeDolares;
+        private double valorReais;
+
+        // Métodos construtores
+        public ConversorDolar()
+        {
+            this.cotacao = 0;
+            this.quantidadeDolares = 0;
+            this.valorReais = 0;
+        }
+        public ConversorDolar(double cotacao, double quantidadeDolares)
+        {
+            this.cotacao = cotacao;
+            this.quantidadeDolares = quantidadeDolares;
+            this.valorReais = 0;
+        }
+
+        // Métodos de interface (set/get)
+        public void setCotacao(double cotacao)
+        {
+            this.cotacao = cotacao;
+        }
+        public void setQuantidadeDolares(double quantidadeDolares)
+        {
+            this.quantidadeDolares = quantidadeDolares;
+        }
+        public double getCotacao()
+        {
+            return this.cotacao;
+        }
+        public double getQuantidadeDolares()
+        {
+            return this.quantidadeDolares;
+        }
+        public double getValorReais()
+        {
+            return this.valorReais;
+        }
+        // Método funcional
+        public void calcular()
+        {
+            this.valorReais = this.quantidadeDolares * this.cotacao;
+        }
+    }
+}
---
+++ lista_poo6/lista_poo6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista_poo6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConversorDolar conversor1;
+            conversor1 = new ConversorDolar();
+
+            conversor1.setCotacao(entrarDouble("Digite a cotação do dólar: "));
+            conversor1.setQuantidadeDolares(entrarDouble("Digite a quantidade de dólares: "));
+
+            conversor1.calcular();
+            Console.WriteLine("{0} dólares equivalem a R$ {1}",
+                conversor1.getQuantidadeDolares(),
+                conversor1.getValorReais());
+        }
+
+
+
+        static double entrarDouble(string mensagem)
+        {
+            Console.Write(mensagem);
+            return double.Parse(Console.ReadLine());
+        }
+    }
+}
