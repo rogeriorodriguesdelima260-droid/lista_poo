30b175194f2921aadf27a82a32ed5ad8aeede391
rogeriorodriguesdelima260-droid <rogeriorodriguesdelima260@gmail.com>
2026-09-14 20:24:25 -0300

    Add files via upload

---
+++ lista_poo2/lista_poo2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista_poo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadrado quadrado1;
+            quadrado1 = new Quadrado();
+
+            quadrado1.setAresta(entrarDouble("Digite a aresta do quadrado: "));
+
+            quadrado1.calcular();
+            Console.WriteLine("A área do quadrado de aresta {0} é {1}",
+                quadrado1.getAresta(),
+                quadrado1.getArea());
+        }
+
+
+
+        static double entrarDouble(string mensagem)
+        {
+            Console.Write(mensagem);
+            return double.Parse(Console.ReadLine());
+        }
+    }
+}
