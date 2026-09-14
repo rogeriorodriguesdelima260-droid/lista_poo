90b4bddb7800fc4e94c96045995555685c6808f8
rogeriorodriguesdelima260-droid <rogeriorodriguesdelima260@gmail.com>
2026-09-14 20:38:16 -0300

    Add files via upload

---
+++ lista_poo5/lista_poo5/ConversorMilhas.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista_poo5
+{
+    internal class ConversorMilhas
+    {
+        // Atributos
+        private double milhas;
+        private double quilometros;
+
+        // Métodos construtores
+        public ConversorMilhas()
+        {
+            this.milhas = 0;
+            this.quilometros = 0;
+        }
+        public ConversorMilhas(double milhas)
+        {
+            this.milhas = milhas;
+            this.quilometros = 0;
+        }
+
+        // Métodos de interface (set/get)
+        public void setMilhas(double milhas)
+        {
+            this.milhas = milhas;
+        }
+        public double getMilhas()
+        {
+            return this.milhas;
+        }
+        public double getQuilometros()
+        {
+            return this.quilometros;
+        }
+        // Método funcional
+        public void calcular()
+        {
+            this.quilometros = (this.milhas * 1852) / 1000;
+        }
+    }
+}
---
+++ lista_poo5/lista_poo5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista_poo5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConversorMilhas conversor1;
+            conversor1 = new ConversorMilhas();
+
+            conversor1.setMilhas(entrarDouble("Digite o valor em milhas marítimas: "));
+
+            conversor1.calcular();
+            Console.WriteLine("{0} milhas marítimas equivalem a {1} quilômetros",
+                conversor1.getMilhas(),
+                conversor1.getQuilometros());
+        }
+
+
+
+        static double entrarDouble(string mensagem)
+        {
+            Console.Write(mensagem);
+            return double.Parse(Console.ReadLine());
+        }
+    }
+}
