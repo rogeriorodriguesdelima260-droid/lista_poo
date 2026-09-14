04e229d8e5a4ab9e0eb527496644122b426fe464
rogeriorodriguesdelima260-droid <rogeriorodriguesdelima260@gmail.com>
2026-09-14 20:58:21 -0300

    Add files via upload

---
+++ lista_poo7/lista_poo7/Comparador.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista_poo7
+{
+    internal class Comparador
+    {
+        // Atributos
+        private double valor1;
+        private double valor2;
+        private double maior;
+
+        // Métodos construtores
+        public Comparador()
+        {
+            this.valor1 = 0;
+            this.valor2 = 0;
+            this.maior = 0;
+        }
+        public Comparador(double valor1, double valor2)
+        {
+            this.valor1 = valor1;
+            this.valor2 = valor2;
+            this.maior = 0;
+        }
+
+        // Métodos de interface (set/get)
+        public void setValor1(double valor1)
+        {
+            this.valor1 = valor1;
+        }
+        public void setValor2(double valor2)
+        {
+            this.valor2 = valor2;
+        }
+        public double getValor1()
+        {
+            return this.valor1;
+        }
+        public double getValor2()
+        {
+            return this.valor2;
+        }
+        public double getMaior()
+        {
+            return this.maior;
+        }
+        // Método funcional
+        public void calcular()
+        {
+            if (this.valor1 > this.valor2)
+            {
+                this.maior = this.valor1;
+            }
+            else
+            {
+                this.maior = this.valor2;
+            }
+        }
+    }
+}
---
+++ lista_poo7/lista_poo7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista_poo7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Comparador comparador1;
+            comparador1 = new Comparador();
+
+            comparador1.setValor1(entrarDouble("Digite o 1º valor: "));
+            comparador1.setValor2(entrarDouble("Digite o 2º valor: "));
+
+            comparador1.calcular();
+            Console.WriteLine("O maior valor entre {0} e {1} é {2}",
+                comparador1.getValor1(),
+                comparador1.getValor2(),
+                comparador1.getMaior());
+        }
+
+
+
+        static double entrarDouble(string mensagem)
+        {
+            Console.Write(mensagem);
+            return double.Parse(Console.ReadLine());
+        }
+    }
+}
