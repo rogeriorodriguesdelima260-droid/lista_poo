6e40cc1062c491e994d5e1df0802b8615c97470f
rogeriorodriguesdelima260-droid <rogeriorodriguesdelima260@gmail.com>
2026-09-14 21:04:37 -0300

    Add files via upload

---
+++ lista_poo9/lista_poo9/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista_poo9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo1;
+            retangulo1 = new Retangulo();
+
+            retangulo1.setBase(entrarDouble("Digite a base do retângulo: "));
+            retangulo1.setAltura(entrarDouble("Digite a altura do retângulo: "));
+
+            retangulo1.calcular();
+
+            Console.WriteLine("A área do retângulo de base {0} e altura {1} é {2}",
+                retangulo1.getBase(),
+                retangulo1.getAltura(),
+                retangulo1.getArea());
+
+            if (retangulo1.getArea() > 100)
+            {
+                Console.WriteLine("Terreno grande");
+            }
+            else
+            {
+                Console.WriteLine("Terreno pequeno");
+            }
+        }
+
+
+
+        static double entrarDouble(string mensagem)
+        {
+            Console.Write(mensagem);
+            return double.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ lista_poo9/lista_poo9/Retangulo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista_poo9
+{
+    internal class Retangulo
+    {
+        // Atributos
+        private double base_;
+        private double altura;
+        private double area;
+
+        // Métodos construtores
+        public Retangulo()
+        {
+            this.base_ = 0;
+            this.altura = 0;
+            this.area = 0;
+        }
+        public Retangulo(double base_, double altura)
+        {
+            this.base_ = base_;
+            this.altura = altura;
+            this.area = 0;
+        }
+
+        // Métodos de interface (set/get)
+        public void setBase(double base_)
+        {
+            this.base_ = base_;
+        }
+        public void setAltura(double altura)
+        {
+            this.altura = altura;
+        }
+        public double getBase()
+        {
+            return this.base_;
+        }
+        public double getAltura()
+        {
+            return this.altura;
+        }
+        public double getArea()
+        {
+            return this.area;
+        }
+        // Método funcional
+        public void calcular()
+        {
+            this.area = this.base_ * this.altura;
+        }
+    }
+}
